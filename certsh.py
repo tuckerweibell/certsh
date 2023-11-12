f8e61d6d0b91e0cc444d237835acf5d3c429e6b4
tuckerweibell <104028366+tuckerweibell@users.noreply.github.com>
2023-11-12 13:13:23 -0700

    Create certsh.py

---
+++ certsh.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import pycurl
+import re
+from io import BytesIO
+
+DOMAIN = input("Enter domain: ")
+
+
+curl = pycurl.Curl()
+curl.setopt(curl.URL, f"https://crt.sh/?q={DOMAIN}")
+
+buffer = BytesIO()
+curl.setopt(curl.WRITEDATA, buffer)
+
+curl.perform()
+response = buffer.getvalue()
+res = response.decode('utf-8')
+
+regex = r'(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}'
+
+domains = re.findall(regex, res)
+
+uniq = set()
+
+for domain in domains:
+  if DOMAIN in domain:
+    uniq.add(domain)
+
+final = sorted(uniq)
+
+for domain in final:
+  print(domain)
