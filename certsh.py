46f4ed3674f9ab27ee1b44b0650854d7ffb3de2c
Tucker Weibell <104028366+tuckerweibell@users.noreply.github.com>
2025-05-29 22:21:02 -0600

    Update certsh.py

--- certsh.py
+++ certsh.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
-ROCK ON!
-
 import pycurl
 import re
+import sys
 from io import BytesIO
 
-DOMAIN = input("Enter domain: ")
+if len(sys.argv) < 2:
+    print("Usage: certsh <domain>")
+    sys.exit(1)
 
+DOMAIN = sys.argv[1]
 
 curl = pycurl.Curl()
 curl.setopt(curl.URL, f"https://crt.sh/?q={DOMAIN}")
@@ -16,20 +18,20 @@ buffer = BytesIO()
 curl.setopt(curl.WRITEDATA, buffer)
 
 curl.perform()
-response = buffer.getvalue()
-res = response.decode('utf-8')
+curl.close()
 
-regex = r'(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}'
+response = buffer.getvalue().decode('utf-8')
 
-domains = re.findall(regex, res)
+regex = r'(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}'
+domains = re.findall(regex, response)
 
 uniq = set()
 
 for domain in domains:
-  if DOMAIN in domain:
-    uniq.add(domain)
+    if DOMAIN in domain:
+        uniq.add(domain)
 
 final = sorted(uniq)
 
 for domain in final:
-  print(domain)
+    print(domain)
